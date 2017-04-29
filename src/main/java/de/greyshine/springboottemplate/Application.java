2976c300d4596d14ceb5a8eca86bbfd3539222bb
Dirk Schumacher <greyshine@Dirks-MacbookAir.fritz.box>
2017-04-29 18:24:25 +0200

    invented interceptor example

--- src/main/java/de/greyshine/springboottemplate/Application.java
+++ src/main/java/de/greyshine/springboottemplate/Application.java
@@ -1,17 +1,27 @@
 package de.greyshine.springboottemplate;
 
+
+import java.util.Map;
+
 import javax.servlet.ServletContext;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.PathMatcher;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 
 @SpringBootApplication
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
 	
 	@Bean
 	public SpringTemplateEngine templateEngine(ServletContext inServletContext) {
-	    return new de.greyshine.springboottemplate.TemplateEngine();
+		return new de.greyshine.springboottemplate.TemplateEngine();
 	}
-
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new ExampleInterceptor()).addPathPatterns( "/*" );
+	}
 }
---
+++ src/main/java/de/greyshine/springboottemplate/ExampleInterceptor.java
@@ -0,0 +1,51 @@
+package de.greyshine.springboottemplate;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * It is also good to extend org.springframework.web.servlet.handler.HandlerInterceptorAdapter
+ * @author greyshine
+ *
+ */
+public class ExampleInterceptor implements AsyncHandlerInterceptor {
+	
+	public ExampleInterceptor() {
+		System.out.println( new Throwable().getStackTrace()[0].getClassName() +" instantiated" );
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		System.out.println( "here: "+ new Throwable().getStackTrace()[0] );
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+		System.out.println( "here: "+ new Throwable().getStackTrace()[0] );
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		
+		System.out.println( "here: "+ new Throwable().getStackTrace()[0] );
+		
+	}
+
+	@Override
+	public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		System.out.println( "here: "+ new Throwable().getStackTrace()[0] );
+	}
+
+
+}
